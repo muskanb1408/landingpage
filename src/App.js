e33ca6e9c152685942e5505214e0afbbb845eef9
muskanb1408 <70267379+muskanb1408@users.noreply.github.com>
2020-09-23 15:29:31 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import "./App.css";
+
+class App extends Component{
+  
+render(){
+
+
+  return(
+
+<div>
+<div className="nav">
+  <ul><li className="brand">Atlassian</li>
+  <li>Products</li>
+  <li>Teams</li>
+  <li>Support</li>
+  <li>Try</li>
+  <li>Buy</li></ul>
+</div>
+<div className="content">
+  <h1 className="heading">Team Up</h1>
+  <p className="para">to create what's next</p>
+  <p className="icon"> Play video</p>
+</div>
+
+
+</div>
+
+  ) ;
+}
+
+}
+
+
+
+
+export default App;
